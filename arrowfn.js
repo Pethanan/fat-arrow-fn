a6a4f6aa9e9c817be721243c3befeac9ecc48892
Pethanan <105306532+Pethanan@users.noreply.github.com>
2022-12-13 23:18:42 +0530

    Create arrowfn.js

---
+++ arrowfn.js
@@ -0,0 +1,50 @@
+class Student
+{
+static count = 0; 
+static countObjects()
+{
+console.log(`${Student.count} objects created`);
+  
+}
+
+constructor(name, age, ph, bmarks)
+{
+Student.count++;
+this.name = name;
+this.age=age;
+this.ph = ph;
+this.bmarks = bmarks; 
+}
+
+eligibility(elScore)
+{
+return (elAge) => {
+  return this.bmarks>=40 && this.age>=elAge ? true : false;}
+}
+}
+
+
+
+let karthik = new Student("Karthik", 20, 8888888888, 30);
+
+
+
+let Vignesh = new Student("Vignesh", 22, 8888888888, 43);
+
+
+
+let Ravi = new Student("Ravi", 23, 8888888888, 60);
+
+
+
+let Anand = new Student("Anand", 42, 8888888888, 60);
+
+
+
+let Vijay = new Student("Vijay", 25, 8888888888, 20);
+
+console.log(karthik.eligibility(40)(20))
+console.log(Vignesh.eligibility(40)(20))
+console.log(Ravi.eligibility(40)(20))
+console.log(Anand.eligibility(40)(20))
+console.log(Vijay.eligibility(40)(20))
